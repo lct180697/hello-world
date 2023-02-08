508db112363d34fc382e9b51a681c52ca2965a22
lct180697 <lct1806@gmail.com>
2023-02-08 11:06:22 +0700

    tunglc

---
+++ src/main/java/stackjava/com/mavenhello/Hello.java
@@ -0,0 +1,11 @@
+package stackjava.com.mavenhello;
+
+import org.apache.commons.text.WordUtils;
+
+public class Hello {
+	public static void main(String[] args) {
+		String message = "hello world";
+		System.out.println(WordUtils.capitalize(message));
+	}
+
+}
